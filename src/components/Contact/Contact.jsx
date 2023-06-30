fad2a170b3f779a481a95b2c2942c272eebec5fe
miftahullislam <miftahullislamsohan@gmail.com>
2023-06-30 20:44:44 +0600

    Contact complated

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -3,9 +3,26 @@ import './Contact.css'
 import {MdOutlineEmail} from 'react-icons/md'
 import {RiMessengerLine} from 'react-icons/ri'
 import {BsWhatsapp} from 'react-icons/bs'
-
+import { useRef } from 'react';
+import emailjs from 'emailjs-com';
 
 const Contact = () => {
+    const form = useRef();
+
+
+    const sendEmail = (e) => {
+        e.preventDefault();
+        
+        emailjs.sendForm('service_cgke0gd', 'template_9t9ad78', form.current, 'sKrTOOUT-I9rbSBZ3')
+        
+          .then((result) => {
+              console.log(result.text);
+          }, (error) => {
+              console.log(error.text);
+          });
+          e.target.reset();
+      };
+    
     return (
         <section id='contact'>
             <h5>Get In Touch</h5>
@@ -14,29 +31,32 @@ const Contact = () => {
             <div className="container contact_container">
                 <div className="contact_options">
                     <article className="contact_option">
-                        <MdOutlineEmail />
+                        <MdOutlineEmail className='contact_option-icon'/>
                         <h4>Email</h4>
                         <h5>miftahullislamsohan@gmail.com</h5>
                         <a href="mailto:miftahullislamsohan@gmail.com" target='_blank'>Send a Massage</a>
                     </article>
                     <article className="contact_option">
-                        <RiMessengerLine />
+                        <RiMessengerLine className='contact_option-icon'/>
                         <h4>Messenger</h4>
                         <h5>miftahullislam@gmail.com</h5>
                         <a href="https://m.me/miftahulislam2001" target='_blank'>Send a Massage</a>
                     </article>
                     <article className="contact_option">
-                    <BsWhatsapp />
+                    <BsWhatsapp className='contact_option-icon'/>
                         <h4>WhatsApp</h4>
                         <h5>miftahullislam@gmail.com</h5>
                         <a href="https://api.whatsapp.com/send?phone+8801633615258" target='_blank'>Send a Massage</a>
                     </article>
 
-
-                    <form>
-
-                    </form>
                 </div>
+                
+                <form ref={form} onSubmit={sendEmail}>
+                        <input type="text" name='name' placeholder='Your Full name' required />
+                        <input type="email" name='email' placeholder='Your Email' required />
+                        <textarea name="message" rows="7" placeholder='Your message' required></textarea>
+                        <button type="submit" className='btn btn-primary'>Send Message</button>
+                    </form>
             </div>
         </section>
     );
