56ddd60b419f85ab1d353993e4c8bad837b258c4
miftahullislam <miftahullislamsohan@gmail.com>
2023-06-30 15:48:17 +0600

    Portfolio Section complated

--- src/components/Portfolio/Portfolio.jsx
+++ src/components/Portfolio/Portfolio.jsx
@@ -1,10 +1,81 @@
-import React from 'react';
-import './Portfolio.css'
+import React from "react";
+import "./Portfolio.css";
+import img_1 from "../../assets/img_1.png";
 
 const Portfolio = () => {
     return (
-        <section id='portfolio'>
-            <h5>Portfolio Section</h5>
+        <section id="portfolio">
+            <h5>My Recent Work</h5>
+            <h2>Portfolio</h2>
+
+            <div className="container portfolio_container">
+                <article className="portfolio_item">
+                    <div className="portfolio_item-img">
+                        <img src={img_1} alt="" />
+                    </div>
+                    <h3>Portfolio item title</h3>
+                    <div className="portfolio_item-cta">
+                    <a
+                        href="https://github.com/Miftahulislam2001/martail-arts-client"
+                        className="btn"
+                        target="_blank"
+                    >
+                        Github
+                    </a>
+                    <a
+                        href="https://martial-arts-5150e.web.app"
+                        className="btn btn-primary"
+                        target="_blank"
+                    >
+                        Live Demo
+                    </a>
+                    </div>
+                </article>
+                <article className="portfolio_item">
+                    <div className="portfolio_item-img">
+                        <img src={img_1} alt="" />
+                    </div>
+                    <h3>Portfolio item title</h3>
+                    <div className="portfolio_item-cta">
+                    <a
+                        href="https://github.com/Miftahulislam2001/martail-arts-client"
+                        className="btn"
+                        target="_blank"
+                    >
+                        Github
+                    </a>
+                    <a
+                        href="https://martial-arts-5150e.web.app"
+                        className="btn btn-primary"
+                        target="_blank"
+                    >
+                        Live Demo
+                    </a>
+                    </div>
+                </article>
+                <article className="portfolio_item">
+                    <div className="portfolio_item-img">
+                        <img src={img_1} alt="" />
+                    </div>
+                    <h3>Portfolio item title</h3>
+                    <div className="portfolio_item-cta">
+                    <a
+                        href="https://github.com/Miftahulislam2001/martail-arts-client"
+                        className="btn"
+                        target="_blank"
+                    >
+                        Github
+                    </a>
+                    <a
+                        href="https://martial-arts-5150e.web.app"
+                        className="btn btn-primary"
+                        target="_blank"
+                    >
+                        Live Demo
+                    </a>
+                    </div>
+                </article>
+            </div>
         </section>
     );
 };
--- src/components/Services/Services.jsx
+++ src/components/Services/Services.jsx
@@ -1,10 +1,112 @@
 import React from 'react';
 import './Services.css'
+import {BsCheckLg} from 'react-icons/bs'
 
 const Services = () => {
     return (
         <section id='services'>
-            <h5>Services Section</h5>
+            <h5>What I Offer</h5>
+            <h2>Services</h2>
+
+            <div className=" container services_container">
+                <article className='service'>
+                   <div className="service_head">
+                        <h3>UI/UX Design</h3>
+                    </div> 
+                    <ul className='service_list'>
+                        <li>
+                            <BsCheckLg />
+                            <p>Lorem ipsum dolor sit amet consectetur elit.</p>
+                        </li>
+                        <li>
+                            <BsCheckLg />
+                            <p>Lorem ipsum dolor sit amet consectetur elit.</p>
+                        </li>
+                        <li>
+                            <BsCheckLg />
+                            <p>Lorem ipsum dolor sit amet consectetur elit.</p>
+                        </li>
+                        <li>
+                            <BsCheckLg />
+                            <p>Lorem ipsum dolor sit amet consectetur elit.</p>
+                        </li>
+                        <li>
+                            <BsCheckLg />
+                            <p>Lorem ipsum dolor sit amet consectetur elit.</p>
+                        </li>
+                    
+                        <li>
+                            <BsCheckLg />
+                            <p>Lorem ipsum dolor sit amet consectetur elit.</p>
+                        </li>
+                    </ul>
+                </article>
+                {/* Web development  */}
+                <article className='service'>
+                   <div className="service_head">
+                        <h3>Web Development</h3>
+                    </div> 
+                    <ul className='service_list'>
+                        <li>
+                            <BsCheckLg />
+                            <p>Lorem ipsum dolor sit amet consectetur elit.</p>
+                        </li>
+                        <li>
+                            <BsCheckLg />
+                            <p>Lorem ipsum dolor sit amet consectetur elit.</p>
+                        </li>
+                        <li>
+                            <BsCheckLg />
+                            <p>Lorem ipsum dolor sit amet consectetur elit.</p>
+                        </li>
+                        <li>
+                            <BsCheckLg />
+                            <p>Lorem ipsum dolor sit amet consectetur elit.</p>
+                        </li>
+                        <li>
+                            <BsCheckLg />
+                            <p>Lorem ipsum dolor sit amet consectetur elit.</p>
+                        </li>
+                    
+                        <li>
+                            <BsCheckLg />
+                            <p>Lorem ipsum dolor sit amet consectetur elit.</p>
+                        </li>
+                    </ul>
+                </article>
+                <article className='service'>
+                   <div className="service_head">
+                        <h3>Content Creator</h3>
+                    </div> 
+                    <ul className='service_list'>
+                        <li>
+                            <BsCheckLg />
+                            <p>Lorem ipsum dolor sit amet consectetur elit.</p>
+                        </li>
+                        <li>
+                            <BsCheckLg />
+                            <p>Lorem ipsum dolor sit amet consectetur elit.</p>
+                        </li>
+                        <li>
+                            <BsCheckLg />
+                            <p>Lorem ipsum dolor sit amet consectetur elit.</p>
+                        </li>
+                        <li>
+                            <BsCheckLg />
+                            <p>Lorem ipsum dolor sit amet consectetur elit.</p>
+                        </li>
+                        <li>
+                            <BsCheckLg />
+                            <p>Lorem ipsum dolor sit amet consectetur elit.</p>
+                        </li>
+                    
+                        <li>
+                            <BsCheckLg />
+                            <p>Lorem ipsum dolor sit amet consectetur elit.</p>
+                        </li>
+                    </ul>
+                </article>
+            </div>
         </section>
     );
 };
