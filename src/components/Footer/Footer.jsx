e475307dbf2a1809409bdb0a9b42db901902bc29
miftahullislam <miftahullislamsohan@gmail.com>
2023-07-01 10:25:52 +0600

    footer complated

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,11 +1,36 @@
 import React from 'react';
 import './Footer.css'
+import {FaFacebookF} from 'react-icons/fa'
+import {FiInstagram} from 'react-icons/fi'
+import {FiTwitter} from 'react-icons/fi'
 
 const Footer = () => {
     return (
-        <div>
-            <h5>Footer Section</h5>
-        </div>
+       
+        <footer>
+            <a href="#" className='footer_logo'>Sohan</a>
+
+            <ul className='permalink'>
+             <li><a href="#">Home</a></li>
+             <li><a href="#about">About</a></li>
+             <li><a href="#experience">Experience</a></li>
+             <li><a href="#services">Services</a></li>
+             <li><a href="#portfolio">Portfolio</a></li>
+             <li><a href="#testimonials">Testimonials</a></li>
+             <li><a href="#contact">Contact</a></li>
+            </ul>
+
+            <div className="footer_social">
+             <a href="https://facebook.com"><FaFacebookF /></a>
+             <a href="https://instagram.com"><FiInstagram /></a>
+             <a href="https://twitter.com"><FiTwitter /></a>
+            </div>
+
+            <div className="footer_copyright">
+                <small>&copy; Miftahul Islam Sohan.All rights Reserved</small>
+            </div>
+
+        </footer>
     );
 };
 
